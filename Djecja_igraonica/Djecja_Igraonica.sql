0826ddbc001e8f03dd7c0358055c3e9520680d05
Patrikgomercic1 <131706932+Patrikgomercic1@users.noreply.github.com>
2023-06-08 21:35:57 +0200

    Insert into
    Dodane su 3 vrijednosti u 3 grupe i napravljen update za jednu tablicu

--- Djecja_igraonica/Djecja_Igraonica.sql
+++ Djecja_igraonica/Djecja_Igraonica.sql
@@ -33,3 +33,26 @@ create table dijete_skupina(
 alter table skupina add foreign key (teta) references teta(sifra);
 alter table dijete_skupina add foreign key (dijete) references dijete(sifra);
 alter table dijete_skupina add foreign key (skupina) references skupina(sifra);
+
+
+insert into teta(sifra,ime,prezime)
+values
+	(1,'Ivana','Sokoliæ'),
+	(2,'Gorana','Majiæ'),
+	(3,'Maja','Antunoviæ');
+
+insert into dijete(sifra,ime,prezime)
+values
+	(1,'Ivan','Horvat'),
+	(2,'Luka','Babiæ'),
+	(3,'Marija','Kusturiæ');
+
+insert into skupina(sifra,naziv,teta)
+values
+	(1,'Skupina_1','2'),
+	(2,'Skupina_2','3'),
+	(3,'Skupina_3','1');
+
+update skupina set naziv='Tigar' where sifra=1
+
+--drop database if exists djecja_igraonica;
--- Djecji_vrtic/Djecji_Vrtic.sql
+++ Djecji_vrtic/Djecji_Vrtic.sql
@@ -29,10 +29,34 @@ create table odgajateljica(
 create table strucnasprema(
 	sifra int not null primary key identity(1,1),
 	naziv varchar(50)
-	
 );
 
+create table dijete_skupina(
+	dijete int,
+	skupina int
+);
+
+
 alter table odgajateljica add foreign key (strucnasprema) references strucnasprema(sifra);
-alter table dijete add foreign key (skupina) references skupina(sifra);
 alter table dijete_skupina add foreign key (dijete) references dijete(sifra);
+alter table dijete_skupina add foreign key (skupina) references skupina(sifra);
 alter table skupina add foreign key (odgajateljica) references odgajateljica(sifra);
+
+
+
+insert into strucnasprema(sifra,naziv)
+values (1,'odgajateljica');
+
+insert into odgajateljica (sifra,ime,prezime,strucnasprema)
+values 
+	(1,'Glorija','Matijeviæ','1'),
+	(2,'Marta','Radiæ','1'),
+	(3,'Ivona','Radiæ','1');
+
+	insert into skupina (sifra,naziv,odgajateljica)
+values 
+	(1,'Bubamare','2'),
+	(2,'Leptiriæi','1'),
+	(3,'Pèelice','3');
+
+--drop database if exists DjecjiVrtic;
